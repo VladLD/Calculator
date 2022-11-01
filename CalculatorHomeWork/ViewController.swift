d30dad719be55f68d5700946590ed9ea8a15522b
Vlad <vladyslav.lapchynskyi@gmail.com>
2022-11-01 15:45:12 +0100

    Initial Commit

---
+++ CalculatorHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorHomeWork
+//
+//  Created by Vlad Lapchynskyi on 01.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
